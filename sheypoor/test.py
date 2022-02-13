507f0beab9c651524198800e0262950759510165
Mostafa <m.n.irib@gmail.com>
2022-02-13 19:12:13 +0330

    completed preprocessing steps, change test.ipynb to main.ipynb, added Golestan dataset, added sheypoor dataset,

---
+++ sheypoor/test.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import pytesseract
+import numpy as np
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+filename = 'cap2.jpg'
+img1 = np.array(Image.open(filename))
+text = pytesseract.image_to_string(img1)
+
+print(text)
